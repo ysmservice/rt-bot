ad2708bbb77d023039c7578c828324c78729046a
tasuren <tasuren_0618@yahoo.co.jp>
2021-05-22 21:15:50 +0900

    コマンドのみworkerでできるようにした

--- main.py
+++ main.py
@@ -5,23 +5,32 @@ import rtutil
 
 print("Now loading...")
 
-bot = rtutil.RTShardClient(log=True)
+bot = rtutil.RTShardClient(command_prefix="r2!", log=True)
 
 
 with open("data.json", "r") as f:
     bot.data = load(f)
 
 
-@bot.add_event
+@bot.event
 async def on_ready():
     print("Connected")
 
 
-@bot.add_event
+@bot.event
 async def on_message(message):
-    print(message.content)
-    if message.content == "r2!test":
-        await message.channel.send("test")
+    await bot.run_command(message)
+
+@bot.command()
+async def test(ctx):
+    await ctx.reply("test")
+
+@bot.command()
+async def test_worker(ctx):
+    async def yey(ctx):
+        await ctx.reply("From worker")
+    await ctx.reply("From main")
+    await bot.add_queue(yey, [ctx])
 
 
 bot.run(bot.data["token"])
--- rtutil/client.py
+++ rtutil/client.py
@@ -1,6 +1,8 @@
+
 # RT - Shard
 
 import discord
+from discord.ext import commands
 
 from traceback import format_exc
 from os import cpu_count
@@ -14,10 +16,10 @@ TITLES = {
 }
 
 
-class RTShardClient(discord.AutoShardedBot):
+class RTShardClient(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         # 独自のkwargsを取得しておく。
-        self.worker_count = kwargs.get("worker_count", cpu_count() * 2 + 1)
+        self.worker_count = kwargs.get("worker_count", cpu_count())
         self.print_error = kwargs.get("print_error", True)
         self.print_log = kwargs.get("log", False)
 
@@ -35,6 +37,9 @@ class RTShardClient(discord.AutoShardedBot):
         self.loop.run_until_complete(self.setup_worker())
 
     async def setup_worker(self):
+        # Prefix
+        self.prefix = tuple(self.command_prefix)
+
         # Workerの作成する。
         self._print("wt", "Setting now!")
         self.queue = asyncio.Queue()
@@ -45,7 +50,7 @@ class RTShardClient(discord.AutoShardedBot):
             self._print("wt", f"Setting worker {name}...")
         self._print("wt", "Done!")
 
-        self.add_event_hook()
+        self.add_event(self.process_commands)
 
     async def worker(self, name: str):
         # Worker
@@ -56,10 +61,14 @@ class RTShardClient(discord.AutoShardedBot):
                 break
             # キューが回ってきたらイベントを処理する。
             if current_queue:
-                event_type, data = current_queue
+                if len(current_queue) == 2:
+                    event_type, args = current_queue
+                    kwargs = {}
+                elif len(current_queue) == 3:
+                    event_type, args, kwargs = current_queue
                 # イベントを走らせるがもしエラーがおきたらエラー時の関数を呼び出す。
                 try:
-                    await self.run_event(event_type, data)
+                    await self.run_event(event_type, args, kwargs)
                 except Exception as error:
                     await self.on_worker_error(error, name)
                 self.queue.task_done()
@@ -78,126 +87,56 @@ class RTShardClient(discord.AutoShardedBot):
             kwargs={"worker_name": worker_name}
         )
 
-    async def run_event(self, event_type: str, data: list = [],
+    async def run_event(self, event_type: str, args: list = [],
                         kwargs: dict = {}):
         # 登録されたイベントを呼び出す。
+        if event_type == "not_event":
+            await args[0](*args[1:], **kwargs)
         coros = self.events.get(event_type)
         if coros:
             for coro in coros:
-                await coro(*data, **kwargs)
+                await coro(*args, **kwargs)
 
-    def add_event(self, coro):
+    async def add_queue(self, coro, args: list = [], kwargs: dict = {}):
+        await self.queue.put(["not_event", [coro] + args, kwargs])
+
+    async def run_command(self, message):
+        await self.queue.put(["process_commands", [message]])
+
+    async def on_message(self, message):
+        await self.run_command(message)
+
+    def add_event(self, coro, cname=None):
         # イベントの登録デコレータ。
-        # もしコルーチンじゃなかったらTypeErrorを発生させる。
-        if not asyncio.iscoroutinefunction(coro):
-            raise TypeError("登録するイベントはコルーチンにする必要があります。")
-        # イベントを登録する。
-        if coro.__name__ in self.events:
-            self.events[coro.__name__].append(coro)
+        if asyncio.iscoroutinefunction(coro):
+            name = coro.__name__
         else:
-            self.events[coro.__name__] = [coro]
+            raise TypeError("登録するイベントはコルーチンにする必要があります。")
+        if cname:
+            name = cname
+        # イベントを登録する。
+        if name in self.events:
+            self.events[name].append(coro)
+        else:
+            self.events[name] = [coro]
         self._print("rt", f"Event {coro.__name__} registered.")
 
         return coro
 
-    def remove_event(self, coro):
+    def remove_event(self, coro, cname=None):
         # イベントの登録解除デコレータ。
         # もしコルーチンじゃなかったらTypeErrorを発生させる。
-        if not asyncio.iscoroutinefunction(coro):
+        if asyncio.iscoroutinefunction(coro):
+            cname = coro.__name__
+        else:
             raise TypeError("登録するイベントはコルーチンにする必要があります。")
+        if cname:
+            name = cname
         # イベントの登録を解除する。
-        if coro.__name__ in self.events:
-            del self.events[coro.__name__]
+        if name in self.events:
+            del self.events[name][self.events[name].index(coro)]
+            if not self.events[name]:
+                self.events[name]
             self._print("rt", "Event removed.")
         else:
             raise TypeError("そのイベントはまだ登録されていません。")
-
-    # Workerにデータを転送するためのイベントを登録するやつ。
-    def add_event_hook(self):
-        @self.event
-        async def on_ready():
-            self._print("rt", "Connected!")
-            await self.queue.put(["on_ready", []])
-
-        @self.event
-        async def on_message(message):
-            await self.queue.put(["on_message", [message]])
-
-        async def on_reaction(reaction, user, event_type):
-            await self.queue.put(
-                ["on_reaction_" + event_type, [reaction, user]])
-
-        @self.event
-        async def on_reaction_add(reaction, user):
-            await on_reaction(reaction, user, "add")
-
-        @self.event
-        async def on_reaction_remove(reaction, user):
-            await on_reaction(reaction, user, "remove")
-
-        @self.event
-        async def on_raw_reaction_add(payload, event_type="add"):
-            await self.queue.put(["on_raw_reaction_" + event_type, [payload]])
-
-        @self.event
-        async def on_raw_reaction_remove(payload):
-            await on_raw_reaction_add(payload, "remove")
-
-        async def on_member(member, event_type):
-            await self.queue.put(["on_member_" + event_type, [member]])
-
-        @self.event
-        async def on_member_join(member):
-            await on_member(member, "join")
-
-        @self.event
-        async def on_member_remove(member):
-            await on_member(member, "remove")
-
-        @self.event
-        async def on_raw_message_delete(payload):
-            await self.queue.put(["on_raw_message_delete", [payload]])
-
-        @self.event
-        async def on_guild_channel_delete(channel, event_type="delete"):
-            await self.queue.put(["on_guild_channel_" + event_type, [channel]])
-
-        @self.event
-        async def on_guild_channel_create(channel):
-            await on_guild_channel_delete(channel, "create")
-
-        @self.event
-        async def on_guild_channel_update(before, after):
-            await self.queue.put(["on_guild_channel_update", [before, after]])
-
-        @self.event
-        async def on_guild_update(before, after):
-            await self.queue.put(["on_guild_update", [before, after]])
-
-        @self.event
-        async def on_guild_role_create(role, event_type="create"):
-            await self.queue.put(["on_guild_role_" + event_type, [role]])
-
-        @self.event
-        async def on_guild_role_delete(role):
-            await on_guild_role_create(role, "delete")
-
-        @self.event
-        async def on_guild_role_update(before, after):
-            await self.queue.put(["on_guild_role_update", [before, after]])
-
-        @self.event
-        async def on_member_ban(guild, user, event_type="ban"):
-            await self.queue.put(["on_member_" + event_type, [guild, user]])
-
-        @self.event
-        async def on_member_unban(guild, user):
-            await on_member_ban(guild, user, "unban")
-
-        @self.event
-        async def on_invite_create(invite, event_type="create"):
-            await self.queue.put(["on_invite_" + event_type, [invite]])
-
-        @self.event
-        async def on_invite_delete(invite):
-            await on_invite_create(invite, "delete")
