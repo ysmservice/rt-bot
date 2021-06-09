e684c8d007316f60f25f947750ef5a94edb7a23b
tasuren <tasuren_0618@yahoo.co.jp>
2021-06-09 20:44:31 +0900

    toriaezu2

--- rtutil/__init__.py
+++ rtutil/__init__.py
@@ -1,7 +1,8 @@
 # RT - Util
 
-from discord import *
+from discord import * # noqa
 from .backend import * # noqa
 from .worker import * # noqa
 from .cog import * # noqa
 from .utils import * # noqa
+from .errors import * # noqa
