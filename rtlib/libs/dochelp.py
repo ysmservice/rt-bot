9badac10c22e99cdf89918416cb62e04c06a916c
tasuren <tasuren>
2021-07-31 10:26:50 +0900

    DocHelpの作りかけ。

--- rtlib/libs/dochelp.py
+++ rtlib/libs/dochelp.py
@@ -34,14 +34,62 @@ async def soudayo(ctx, mode="便乗"):
 # 上のコマンドにあるドキュメンテーションがヘルプリストに自動で追加されます。"""
 
 from discord.ext import commands
+from typing import Callable
 
 
 class DocHelp(commands.Cog):
+
+    BLANKS = (" ", "\t", "　")
+    HEADDINGS = {
+        "Parameters": "# コマンドの引数",
+        "Examples": "# コマンドの使用例"
+    }
+
     def __init__(self, bot):
         self.bot = bot
         self.data: dict = {}
         if "OnAddRemoveCommand" not in self.bot.cogs:
             self.bot.load_extension("rtlib.libs.on_command_add")
+
+    def remove_blanks(self, text: str, remove_new_line: bool = True) -> str:
+        # 空白を削除する。
+        for blank in self.BLANKS:
+            text = text.replace(blank, "")
+        if remove_new_line:
+            text = text.replace("\n", "")
+        return text
+
+    def convert_normal(self, text: str) -> str:
+        # 文字列の最初にある空白を削除して改行を削除する。
+        if text and text[-1] not in self.BLANKS:
+            while text[0] not in self.BLANKS:
+                text = text[1:]
+            if text[-1] == "\n":
+                text = text[:-1]
+        return text
+
+    def item_parser(self, now_item: str, text: str):
+        if now_item == "Parameters":
+            if now_item
+
+    def parse_doc(self, doc: str, item_parser: Optional[Callable] = None):
+        item_parser = self.item_parser if item_parser is None else item_parser
+        text, now_item, before, now = "", "", "", ""
+
+        for line in doc.splitlines():
+            normal_text, new = self.convert_normal(line), ""
+
+            if all(char == "-" for char in self.remove_blanks(line)):
+                # 項目名
+                new = self.HEADDINGS.get(before, before) + "\n"
+                now_item = before
+            elif normal_text not in self.HEADDINGS:
+                # 項目の中にあるものをitem_parserに通してできたものを追加する。
+                new = item_parser(normal_text) + "\n"
+
+            text += new
+            now += new
+            before = noraml_text
 
     @commands.Cog.listener()
     async def on_command_add(self, command):
