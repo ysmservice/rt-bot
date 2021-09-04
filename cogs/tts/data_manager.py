1f0028ff74a9141b1112c1f9569e42ceccda5952
tasuren <tasuren_0618@yahoo.co.jp>
2021-09-04 13:03:49 +0900

    new: 読み上げのデータマネージャーのバグを修正した。

--- cogs/tts/data_manager.py
+++ cogs/tts/data_manager.py
@@ -1,14 +1,11 @@
 # RT TTS - Data Manager
 
-from rtlib import mysql, DatabaseLocker
+from rtlib import mysql, DatabaseManager
 
 
-MySQLManager, Cursor = mysql.MySQLManager, mysql.Cursor
-
-
-class DataManager(DatabaseLocker):
-    def __init__(self, db: MySQLManager):
-        self.db: MySQLManager = db
+class DataManager(DatabaseManager):
+    def __init__(self, db):
+        self.db = db
 
     async def init_table(self) -> None:
         """テーブルの初期状態を作ります。"""
@@ -17,8 +14,7 @@ class DataManager(DatabaseLocker):
             "id": "BIGINT",
             "content": "TEXT"
         }
-        async with self.db.get_cursor() as cursor:
-            await cursor.create_table("tts", columns)
+        await self.cursor.create_table("tts", columns)
 
     async def _check_exists(self, cursor: Cursor, type_: str, some_id: int):
         """指定されたtypeでsome_idのデータがあるか確認します。
@@ -42,15 +38,14 @@ class DataManager(DatabaseLocker):
             対象のユーザーIDです。
         name : str
             声の名前です。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "voice", user_id):
-                await cursor.update_data(
-                    "tts", {"content": name}, {"type": "voice", "id": user_id}
-                )
-            else:
-                await cursor.insert_data(
-                    "tts", {"type": "voice", "id": user_id, "content": name}
-                )
+        if await self._check_exists(self.cursor, "voice", user_id):
+            await self.cursor.update_data(
+                "tts", {"content": name}, {"type": "voice", "id": user_id}
+            )
+        else:
+            await self.cursor.insert_data(
+                "tts", {"type": "voice", "id": user_id, "content": name}
+            )
 
     async def read_voice(self, user_id: int) -> str:
         """指定したユーザーIDの声を取得します。
@@ -59,17 +54,16 @@ class DataManager(DatabaseLocker):
         ----------
         usre_id : int
             対象のユーザーIDです。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "voice", user_id):
-                row = await cursor.get_data(
-                    "tts", {"type": "voice", "id": user_id}
-                )
-                if row:
-                    return row[-1]
-                else:
-                    return "mei"
+        if await self._check_exists(self.cursor, "voice", user_id):
+            row = await self.cursor.get_data(
+                "tts", {"type": "voice", "id": user_id}
+            )
+            if row:
+                return row[-1]
             else:
                 return "mei"
+        else:
+            return "mei"
 
     async def write_dictionary(self, data: dict, guild_id: int) -> None:
         """辞書を指定したIDと一緒に保存します。
@@ -80,15 +74,14 @@ class DataManager(DatabaseLocker):
             保存する辞書データです。
         guild_id : int
             サーバーのIDです。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "dictionary", guild_id):
-                await cursor.update_data(
-                    "tts", {"content": data}, {"type": "dictionary", "id": guild_id}
-                )
-            else:
-                await cursor.insert_data(
-                    "tts", {"type": "dictionary", "id": guild_id, "content": data}
-                )
+        if await self._check_exists(self.cursor, "dictionary", guild_id):
+            await self.cursor.update_data(
+                "tts", {"content": data}, {"type": "dictionary", "id": guild_id}
+            )
+        else:
+            await self.cursor.insert_data(
+                "tts", {"type": "dictionary", "id": guild_id, "content": data}
+            )
 
     async def read_dictionary(self, guild_id: int) -> dict:
         """指定したIDと一緒に保存されてる辞書を読み込みます。
@@ -97,66 +90,61 @@ class DataManager(DatabaseLocker):
         ----------
         guild_id : int
             サーバーIDです。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "dictionary", guild_id):
-                rows = await cursor.get_data("tts", {"type": "dictionary", "id": guild_id})
-                if rows:
-                    return rows[-1]
-                else:
-                    return {}
+        if await self._check_exists(self.cursor, "dictionary", guild_id):
+            rows = await self.cursor.get_data("tts", {"type": "dictionary", "id": guild_id})
+            if rows:
+                return rows[-1]
             else:
                 return {}
+        else:
+            return {}
 
     async def write_routine_mode(self, user_id: int, b: bool) -> None:
         """指定したユーザーIDのネタモードの切り替えをします。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "routine", user_id):
-                await cursor.update_data(
-                    "tts", {"content": str(int(b))},
-                    {"type": "routine", "id": user_id}
-                )
-            else:
-                await cursor.insert_data(
-                    "tts", {"content": str(int(b)), "type": "routine",
-                            "id": user_id}
-                )
+        if await self._check_exists(self.cursor, "routine", user_id):
+            await self.cursor.update_data(
+                "tts", {"content": str(int(b))},
+                {"type": "routine", "id": user_id}
+            )
+        else:
+            await self.cursor.insert_data(
+                "tts", {"content": str(int(b)), "type": "routine",
+                        "id": user_id}
+            )
 
     async def read_routine_mode(self, user_id: int) -> bool:
         """指定したユーザーのIDのネタモードを調べます。"""
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "routine", user_id):
-                row = await cursor.get_data(
-                    "tts", {"type": "routine", "id": user_id}
-                )
-                if row:
-                    return bool(int(row[-1]))
-                else:
-                    return False
+        if await self._check_exists(self.cursor, "routine", user_id):
+            row = await self.cursor.get_data(
+                "tts", {"type": "routine", "id": user_id}
+            )
+            if row:
+                return bool(int(row[-1]))
             else:
                 return False
+        else:
+            return False
 
     async def write_routine(self, user_id: int, data: dict) -> None:
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "custom", user_id):
-                await cursor.update_data(
-                    "tts", {"content": data},
-                    {"type": "custom", "id": user_id}
-                )
-            else:
-                await cursor.insert_data(
-                    "tts", {"content": data, "type": "custom",
-                            "id": user_id}
-                )
+        if await self._check_exists(self.cursor, "custom", user_id):
+            await self.cursor.update_data(
+                "tts", {"content": data},
+                {"type": "custom", "id": user_id}
+            )
+        else:
+            await self.cursor.insert_data(
+                "tts", {"content": data, "type": "custom",
+                        "id": user_id}
+            )
 
     async def read_routine(self, user_id: int) -> dict:
-        async with self.db.get_cursor() as cursor:
-            if await self._check_exists(cursor, "custom", user_id):
-                row = await cursor.get_data(
-                    "tts", {"type": "custom", "id": user_id}
-                )
-                if row:
-                    return row[-1]
-                else:
-                    return {}
+        if await self._check_exists(self.cursor, "custom", user_id):
+            row = await self.cursor.get_data(
+                "tts", {"type": "custom", "id": user_id}
+            )
+            if row:
+                return row[-1]
             else:
                 return {}
+        else:
+            return {}
