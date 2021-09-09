be651b9bb3b1a6bedd905e1dc3371649ac12b0ea
tasuren <tasuren_0618@yahoo.co.jp>
2021-09-09 09:08:18 +0900

    new: RTが落ちる原因を調べるためのロガーを追加した。

---
+++ cogs/logger.py
@@ -0,0 +1,16 @@
+# RT落ちの情報収集ためのロガー
+
+import discord
+import logging
+
+
+def setup(bot):
+
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.RotatingFileHandler(
+        filename='log/discord.log', encoding='utf-8', mode='w',
+        maxBytes=10000000, backupCount=5
+    )
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
