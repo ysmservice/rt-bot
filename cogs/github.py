089f781486899db90f1c31737451baaf6e8a5eed
tnt <45692264+tuna2134@users.noreply.github.com>
2021-10-01 21:49:27 +0900

    Create github.py
    self.github_tokenの部分にトークン入れといて

---
+++ cogs/github.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import aiohttp
+
+class Github(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.github_token = ""
+        
+    @commands.group(name="github")
+    async def Github(self, ctx):
+        if ctx.invoked_subcommand:
+            return await ctx.send("使い方が間違っています")
+
+    @Github.command(name="issue")
+    async def Issue(self, ctx, title, *, description):
+        data = {
+            "title": title,
+            "body": description,
+        }
+        headers = {
+            "Authorization": "Bearer {}".format(self.github_token)
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post("https://api.github.com/repos/RT-Team/rt-backend/issues",data=data, headers=headers) as r:
+                await ctx.send("issueを登録しました")
+                
+def setup(bot):
+    bot.add_cog(Github(bot))
