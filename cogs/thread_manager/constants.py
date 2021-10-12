10f68746d5c8eee557c072396a6ae7504d35d699
tasuren <tasuren_0618@yahoo.co.jp>
2021-10-12 22:04:27 +0900

    new: スレッドマネージャーを作った。

---
+++ cogs/thread_manager/constants.py
@@ -0,0 +1,6 @@
+# RT.thread_manager - Constants
+
+
+DB = "ThreadManager"    # データベースのテーブル名です。
+MAX_CHANNELS = 10       # 設定できるチャンネル数の最大です。
+ERROR_RANGE = 15        # スレッドが自動アーカイブされたのを検知する際に許容する誤差の範囲です。
