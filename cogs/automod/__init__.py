1ff599c0f1506efa884cf787e7fe8aecc4c1f5a8
tasuren <tasuren_0618@yahoo.co.jp>
2021-10-14 22:20:04 +0900

    updating[help]: ヘルプの書きかけ。

--- cogs/automod/__init__.py
+++ cogs/automod/__init__.py
@@ -41,13 +41,48 @@ class AutoMod(commands.Cog, DataManager):
 
         super(commands.Cog, self).__init__(self)
 
-    @commands.group(aliases=["安全", "モデレーション", "am"])
+    @commands.group(
+        aliases=["安全", "モデレーション", "am"], extras={
+            "headding": {
+                "ja": "スパム対策などのモデレーション機能",
+                "en": "moderation features such as anti-spam"
+            }, "parent": "ServerSafety"
+        }
+    )
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.guild_only()
     async def automod(self, ctx):
         """!lang ja
         --------
-        スパム対策機能や"""
+        スパム対策機能や絵文字制限そして招待可能チャンネル規制などの機能がある自動モデレーション機能です。  
+        これから警告数というワードがでますがこれは誰かがスパムをした際などに加算される警告の数です。  
+        この数がいくつになったらBANやミュートをするといった動作を作ることができます。
+
+        Notes
+        -----
+        スパム対策機能はデフォルトでは警告数が4になったらミュートで7でBANとなります。  
+        そしてスパム検知レベルはデフォルトでは2で二回スパムしたと検知されると警告数が一つ上がります。  
+        これらは設定で変更が可能です。
+
+        Aliases
+        -------
+        am, 安全, モデレーション
+
+        !lane en
+        --------
+        Automatic moderation with anti-spam, emoji restrictions, and invite-only channel restrictions.  
+        This is the number of warnings that will be added when someone spams.  
+        You can create a behavior such as banning or muting when the number reaches this number.
+
+        Notes
+        -----
+        By default, the anti-spam function mutes the user when the number of warnings reaches 4, and bans the user when the number reaches 7.  
+        The spam detection level is 2 by default, and if you are detected as spamming twice, the warning number goes up by one.  
+        These can be changed in the settings.
+
+        Aliases
+        -------
+        am"""
         if not ctx.invoked_subcommand:
             await ctx.reply(
                 {"ja": "使用方法が違います。",
@@ -61,6 +96,21 @@ class AutoMod(commands.Cog, DataManager):
          "en": "It has already set."}
     )
     async def setup_(self, ctx):
+        """!lang ja
+        --------
+        モデレーション機能を有効化します。  
+        これを実行するとスパム対策機能が作動します。  
+        (招待リンク規制や絵文字規制は設定をしないとデフォルトでは有効になりません。)
+
+        Aliases
+        -------
+        設定
+
+        !lang en
+        --------
+        Activate the moderation function.  
+        Doing so will activate the anti-spam feature.  
+        (Invitation link restrictions and emoji restrictions will not be enabled by default unless you set them up.)  """
         await self.setup(ctx.guild.id)
         if ctx.guild.id not in self.guild_cache:
             self.guild_cache.append(ctx.guild.id)
@@ -78,6 +128,17 @@ class AutoMod(commands.Cog, DataManager):
          "en": "Could not find the setting."}
     )
     async def setdown_(self, ctx):
+        """!lang ja
+        --------
+        モデレーションを機能を無効にします。
+
+        Aliases
+        -------
+        終了
+
+        !lang en
+        --------
+        Disables the moderation feature."""
         await self.setdown(ctx.guild.id)
         if ctx.guild.id in self.guild_cache:
             self.guild_cache.remove(ctx.guild.id)
@@ -90,7 +151,21 @@ class AutoMod(commands.Cog, DataManager):
 
     @automod.group(aliases=["w", "警告"])
     async def warn(self, ctx):
-        """"""
+        """!lang ja
+        --------
+        警告数を管理するためのコマンドです。
+
+        Aliases
+        -------
+        w, 警告
+
+        !lang en
+        --------
+        This command is used to manage the number of warnings.
+
+        Aliases
+        -------
+        w"""
         if not ctx.invoked_subcommand:
             await self.automod(ctx)
 
@@ -130,6 +205,41 @@ class AutoMod(commands.Cog, DataManager):
     @check
     @assertion_error_handler(WARN_ERROR)
     async def set_(self, ctx, warn: float, *, target: discord.Member):
+        """!lang ja
+        --------
+        指定したメンバーの警告数を設定します。  
+        設定した場合はその人の警告数がチェックされるため、BANまたはミュートする警告数に設定した警告数が達している場合は処罰が実行されます。  
+        警告数は0以上100以下である必要があります。
+
+        Parameters
+        ----------
+        warn : float
+            設定する警告数です。  
+            小数点を含むじょのにできます。
+        target : メンバーのメンションまたは名前
+            警告数を設定する対象のメンバーです。
+
+        Aliases
+        -------
+        s, 設定
+
+        !lang en
+        --------
+        Set the number of warnings for the specified member.  
+        If set, the number of warnings for that person will be checked, and if the number of warnings set reaches the number of warnings to be banned or muted, the punishment will be executed.  
+        The number of warnings must be between 0 and 100.
+
+        Parameters
+        ----------
+        warn : float
+            The number of warnings to set.  
+            The number of warnings to set, including decimal points.
+        target : member mention or name
+            The target member for which to set the number of warnings.
+
+        Aliases
+        -------
+        s"""
         if warn >= 0:
             guild = await self.update_setting(
                 ctx, {
@@ -145,6 +255,61 @@ class AutoMod(commands.Cog, DataManager):
     @check
     @assertion_error_handler(WARN_ERROR)
     async def mute(self, ctx, warn: float, *, role: discord.Role):
+        """!lang ja
+        --------
+        いくつの警告数になったら何のロールを付与してミュートにするかを設定します。
+
+        Parameters
+        ----------
+        warn : float
+            いくつの警告数になったらミュートにするかです。
+        role : ロールのメンションまたは名前
+            ミュートする際に付与するロールです。
+
+        Warnings
+        --------
+        警告数を低く設定しすぎるとたまたまスパムとして誤検出されただけでミュートなどになりかねません。  
+        これには注意してください。  
+        そしてこれはデフォルトでは無効となっています。  
+        理由は仕組みはロール付与によるミュートだからです。  
+        なので設定をしないとスパムする人がいてもミュートはしません。  
+        (ですがご安心を、その人がスパムし続ける場合はBANされます。)
+
+        Notes
+        -----
+        デフォルトでは4となっています。  
+        また、ロールを付与ではなく剥奪もしたいという場合は`linker`という機能を使ってみましょう。  
+        `rt!help linker`からヘルプを表示できます。
+
+        Aliases
+        -------
+        m, ミュート
+
+        !lang en
+        --------
+        Set the number of warnings to be granted and muted.
+
+        Parameters
+        ----------
+        warn : float
+            How many warnings are there before you mute them?
+        role : The name or mention of the role
+            The role to grant when muting.
+
+        Warnings
+        --------
+        If you set the number of warnings too low, you may end up muting the spam just by chance.
+        Be careful with this.
+        This is disabled by default.
+        The reason is that the mechanism is mute by roll.
+        So if you don't set it up, it won't mute people who spam you.
+        (But don't worry, if they keep spamming, they'll be BAN.)
+
+        Notes
+        -----
+        The default is 4.
+        If you also want to revoke a role rather than grant it, you can use `linker`.
+        `rt!help linker`."""
         await self.update_setting(
             ctx, {
                 "ja": f"ミュートにする警告数を`{warn}`にしました。",
@@ -156,6 +321,8 @@ class AutoMod(commands.Cog, DataManager):
     @check
     @assertion_error_handler(WARN_ERROR)
     async def ban(self, ctx, warn: float):
+        """!lang ja
+        --------"""
         await self.update_setting(
             ctx, {
                 "ja": f"BANをする警告数を`{warn}`にしました。",
@@ -335,7 +502,7 @@ class AutoMod(commands.Cog, DataManager):
         else:
             return guild
 
-    @tasks.loop(hours=14)
+    @tasks.loop(minutes=30)
     async def reset_warn(self):
         # 警告数をリセットするループです。
         for guild_id in self.guild_cache:
