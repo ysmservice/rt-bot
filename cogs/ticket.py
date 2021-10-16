c3aa45a8796ed5856de07a24fc3a0b803a0d9da9
tasuren <tasuren_0618@yahoo.co.jp>
2021-10-16 12:29:37 +0900

    new: チケットメッセージを導入してリアクションからボタンにした。

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -1,27 +1,129 @@
 # RT - Ticket
 
+from typing import TYPE_CHECKING, Union, Optional, Dict
+
 from discord.ext import commands, tasks
 import discord
 
+from rtlib import componesy
 from time import time
 
+if TYPE_CHECKING:
+    from aiomysql import Pool
 
-class Ticket(commands.Cog):
+    class NewInteraction(discord.Interaction):
+        member: Union[discord.Member, discord.User]
+
+
+class RealNewInteraction:
+    def __init__(self, interaction: discord.Interaction):
+        self.interaction = interaction
+        self.member = interaction.user
+
+    def __getattr__(self, name):
+        return getattr(self.interaction, name)
+
+
+# 定数を設定する。
+TITLE = "[Ticket]"
+CUSTOM_ID = "rt_ticket"
+COOLDOWN = 150
+TABLE = "TicketMessage"
+VIEW = componesy.View("TicketView")
+VIEW.add_item(
+    discord.ui.Button, None, label="Ticket",
+    emoji="🎫", custom_id=CUSTOM_ID
+)
+VIEW = VIEW()
+
+
+class DataManager:
+    def __init__(self, pool: "Pool"):
+        self.pool = pool
+
+    async def prepare_table(self) -> None:
+        """テーブルを作成します。"""
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(
+                    f"""CREATE TABLE IF NOT EXISTS {TABLE} (
+                        GuildID BIGINT PRIMARY KEY NOT NULL,
+                        ChannelID BIGINT, Content TEXT
+                    );"""
+                )
+
+    async def set_message(self, channel: discord.TextChannel, content: str) -> None:
+        """チケット作成時に送信するメッセージを設定します。"""
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(
+                    f"""INSERT INTO {TABLE}
+                    VALUES (%s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
+                        ChannelID = %s, Content = %s;""",
+                    (channel.guild.id, channel.id,
+                     content, channel.id, content)
+                )
+
+    async def delete_message(self, guild_id: int) -> None:
+        """指定されたサーバーに設定されているメッセージを削除します。"""
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(
+                    f"SELECT GuildID FROM {TABLE} WHERE GuildID = %s;",
+                    (guild_id,)
+                )
+                assert await cursor.fetchone(), "見つかりませんでした。"
+                await cursor.execute(
+                    f"DELETE FROM {TABLE} WHERE GuildID = %s;",
+                    (guild_id,)
+                )
+
+    async def read(self, guild_id: int) -> Optional[str]:
+        """指定されたサーバーに設定されているメッセージを読み込みます。"""
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(
+                    f"SELECT Content FROM {TABLE} WHERE GuildID = %s;",
+                    (guild_id,)
+                )
+                if (row := await cursor.fetchone()):
+                    return row[0]
+
+
+class Ticket(commands.Cog, DataManager):
     def __init__(self, bot):
         self.bot = bot
-        self.cooldown = {}
+        self.cooldown: Dict[int, float] = {}
+        self.cooldown_killer.start()
+        self.bot.loop.create_task(self.init_database())
+
+    async def init_database(self):
+        # データベースの準備をする。
+        super(commands.Cog, self).__init__(self.bot.mysql.pool)
+        await self.prepare_table()
+
+    def cog_unload(self):
+        self.cooldown_killer.cancel()
+
+    @tasks.loop(minutes=5)
+    async def cooldown_killer(self):
+        # 放置されたクールダウンのキャッシュは削除する。
+        now = time()
+        for mid in list(self.cooldown.keys()):
+            if self.cooldown[mid] <= now:
+                del self.cooldown[mid]
 
     @commands.command(
         extras={
             "headding": {
                 "ja": "チケットチャンネル作成用のパネルを作成します。",
                 "en": "Ticket panel"
-            },
-            "parent": "ServerPanel"
+            }, "parent": "ServerPanel"
         }
     )
     @commands.has_permissions(manage_channels=True)
-    async def ticket(self, ctx, title, description, role: discord.Role = None):
+    async def ticket(self, ctx, title, description, *, role: discord.Role = None):
         """!lang ja
         --------
         チケットチャンネル作成用のパネルを作成します。
@@ -40,8 +142,11 @@ class Ticket(commands.Cog):
         Notes
         -----
         このコマンドはチャンネル管理権限がある人でしか実行できません。  
-        作成されるチケットパネルは🎫のリアクションが付与され、このリアクションを押すことでチケットチャンネルが作成されます。  
-        もしこのパネルを無効化したい場合は単純に作成したパネルを削除すれば良いです。
+        もしこのパネルを無効化したい場合は単純に作成したパネルを削除すれば良いです。  
+        チケットチャンネル作成時に何かメッセージを送信してほしい場合は、チケットのあるチャンネルで以下のコマンドで設定できます。
+        ```
+        rt!tfm メッセージ内容 (もしオフにしたい場合は`off`)
+        ```
 
         Examples
         --------
@@ -64,9 +169,12 @@ class Ticket(commands.Cog):
 
         Notes
         -----
-        This command can only be executed by someone with channel admin rights.  
-        The ticket panel that is created will have a 🎫 reaction, and the ticket channel will be created by pressing this reaction.  
-        If you want to disable this panel, you can simply delete the created panel.
+        This command can only be executed by someone with channel management privileges.  
+        If you want to disable this panel, you can simply delete the panel you created.  
+        If you want some message to be sent when a ticket channel is created, you can set it in the channel with the ticket by using the following command.
+        ```
+        rt!tfm Message content (or `off` if you want to turn it off)
+        ```
 
         Examples
         --------
@@ -77,24 +185,47 @@ class Ticket(commands.Cog):
                 description=description,
                 color=self.bot.colors["normal"]
             )
-            message = await ctx.webhook_send(
+            await ctx.webhook_send(
                 username=ctx.author.name, avatar_url=ctx.author.avatar.url,
                 content=f"RTチケットパネル, {getattr(role, 'id', '...')}",
-                embed=embed, wait=True, replace_language=False
+                embed=embed, wait=True, replace_language=False, view=VIEW
             )
-            await message.add_reaction("🎫")
         else:
             await ctx.reply(
                 {"ja": "このコマンドはカテゴリーにあるテキストチャンネルのみ動作します。",
                  "en": "This command can run on only text channel."}
             )
 
+    @commands.command()
+    @commands.has_permissions(manage_channels=True)
+    async def tfm(self, ctx: commands.Context, *, content: Union[bool, str]):
+        # チケットメッセージ設定用コマンドです。
+        await ctx.trigger_typing()
+        if isinstance(content, bool) and not content:
+            try:
+                await self.delete_message(ctx.guild.id)
+            except AssertionError:
+                return await ctx.reply(
+                    {"ja": "まだチケットメッセージは設定されていません。",
+                     "en": "Ticket message is not set yet."}
+                )
+        else:
+            await self.set_message(ctx.channel, content)
+        await ctx.reply("Ok")
+
     def make_channel_name(self, name: str) -> str:
         # チケットチャンネル用の名前を作る関数です。
         return (name[:90] if len(name) > 90 else name) + "-rtチケット"
 
-    async def on_full_reaction(self, payload):
-        if (str(payload.emoji) != "🎫" or payload.member.bot
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: discord.Interaction):
+        if interaction.data.get("custom_id", "") == CUSTOM_ID:
+            # ボタンによるチケットチャンネル作成もする。
+            await interaction.response.defer()
+            await self.on_ticket(RealNewInteraction(interaction))
+
+    async def on_ticket(self, payload: Union["NewInteraction", discord.RawReactionActionEvent]):
+        if ((hasattr(payload, "emoji") and str(payload.emoji) != "🎫") or payload.member.bot
                 or not payload.message.embeds or not payload.message.guild
                 or not payload.message.content.startswith("RTチケットパネル, ")):
             return
@@ -102,14 +233,18 @@ class Ticket(commands.Cog):
         # リアクションを追加/削除した人の名前でチケットチャンネル名を作る。
         channel_name = self.make_channel_name(payload.member.display_name)
         # リアクションを押した人が既にチャンネルを作成している場合はそのチャンネルを取得する。
-        channel = discord.utils.get(payload.message.guild.text_channels,
-                                    name=channel_name)
+        channel = discord.utils.get(
+            payload.message.guild.text_channels,
+            name=channel_name
+        )
 
-        if payload.event_type == "REACTION_ADD":
+        if channel:
+            # もし既にチケットチャンネルが存在するならそのチャンネルの削除を行う。
+            await channel.delete(reason=f"{TITLE}チケット削除のため。")
+        else:
             # もしリアクションが押されたなら。
             # クールダウンが必要ならチャンネルを作成しない。
-            now = time()
-            if (error := now - self.cooldown.get(payload.member.id, 0)) < 300:
+            if (error := (now := time()) - self.cooldown.get(payload.member.id, 0.0)) < COOLDOWN:
                 await payload.message.channel.send(
                     {"ja": f"{payload.member.mention}, クールダウンが必要なため{error}秒待ってください。",
                      "en": f"{payload.member.mention}, It want cooldown, please wait for {error} seconds."},
@@ -118,16 +253,6 @@ class Ticket(commands.Cog):
             else:
                 self.cooldown[payload.member.id] = now
 
-                if channel:
-                    # もし既にチケットチャンネルが作られているならreturnする。
-                    await payload.message.send(
-                        {"ja": f"{payload.member.mention}, " \
-                            "あなたは既にチケットチャンネルを作成しています。",
-                         "en": f"{payload.member.mention}, " \
-                            "You have already created a ticket channel."},
-                        delete_after=5, target=payload.member.id
-                    )
-                    return
                 # チケットチャンネルの作成に必要な情報を集める。
                 role = (
                     payload.message.guild.get_role(
@@ -151,11 +276,11 @@ class Ticket(commands.Cog):
                 await channel.send(
                     {"ja": f"{payload.member.mention}, ここがあなたのチャンネルです。",
                      "en": f"{payload.member.mention}, Here is your channel!"},
-                    target=payload.member.id
+                    target=payload.member.id, embed=discord.Embed(
+                        title="管理者からのメッセージ", description=first,
+                        color=self.bot.colors["normal"]
+                    ) if (first := await self.read(payload.guild_id)) else None
                 )
-        elif channel:
-            # もしリアクションが削除されたならチャンネルを削除する。
-            await channel.delete()
 
     @commands.Cog.listener()
     async def on_full_reaction_add(self, payload):
