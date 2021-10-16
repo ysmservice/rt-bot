5e5c5c29246959bbc16676eac813e6c12e158e32
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2021-10-16 17:43:51 +0900

    修正
    ・BANコマンドのextrasを指定
    ・キックしたときの文章にBANが入っていたことの修正
    ・引数の説明にインデントをつけた
    ・実行者の名前をBAN・KICK理由につけといた

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,7 +7,13 @@ class Moderation(commands.Cog):
         self.bot = bot
 
     @commands.has_permissions(ban_members=True)
-    @commands.command(aliases=["バン", "ばん", "BAN"])
+    @commands.command(extras={
+        "headding": {
+            "ja": "メンバーのBAN",
+            "en": "BAN members"
+        }, "parent": "ServerSafety"
+    },
+        aliases=["バン", "ばん", "BAN"])
     async def ban(self, ctx, *members: List[discord.Member]):
         """!lang ja
         --------
@@ -16,24 +22,25 @@ class Moderation(commands.Cog):
         Parameters
         ----------
         members : list[mention]
-        誰をBANするかです。空白で区切って複数人指定もできます。"""
+            誰をBANするかです。空白で区切って複数人指定もできます。"""
         excepts = []
         for m in members:
             try:
-                await ctx.guild.ban(m)
+                await ctx.guild.ban(m, reason=f"RTによる実行(実行者:{ctx.author})")
             except:
                 excepts.append(m)
         if len(excepts) == 0:
             await ctx.reply("完了。", delete_after=5)
         else:
-            await ctx.reply(f"BANを実行しました。\n(しかし、{', '.join(excepts)}のBANに失敗しました。)")
+            await ctx.reply(f"BANを実行しました。\n(しかし、{', '.join(excepts)}のBANに失敗しました。)",
+                            delete_after=5)
 
     @commands.has_permissions(kick_members=True)
     @commands.command(extras={
         "headding": {
             "ja": "メンバーのキック",
             "en": "Kick members"
-        }, "parent": ""
+        }, "parent": "ServerSafety"
     },
          aliases=["キック", "きっく", "KICK"])
     async def kick(self, ctx, *members: List[discord.Member]):
@@ -44,17 +51,18 @@ class Moderation(commands.Cog):
         Parameters
         ----------
         members : list[member]
-        誰をキックするかのメンションです。空白で区切って複数人指定もできます。"""
+            誰をキックするかのメンションです。空白で区切って複数人指定もできます。"""
         excepts = []
         for m in members:
             try:
-                await ctx.guild.kick(m)
+                await ctx.guild.kick(m, reason=f"RTによる実行(実行者:{ctx.author})")
             except:
                 excepts.append(m)
         if len(excepts) == 0:
             await ctx.reply("完了。", delete_after=5)
         else:
-            await ctx.reply(f"BANを実行しました。\n(しかし、{', '.join(excepts)}のBANに失敗しました。)")
+            await ctx.reply(f"キックを実行しました。\n(しかし、{', '.join(excepts)}のキックに失敗しました。)",
+                            delete_after=5)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
