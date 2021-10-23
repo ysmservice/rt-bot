9c46f77752e6d9525f9bfebd3eadd90b03b70c0a
tnt <45692264+tuna2134@users.noreply.github.com>
2021-10-24 08:34:59 +0900

    Update autopublic.py

--- cogs/autopublic.py
+++ cogs/autopublic.py
@@ -1,9 +1,27 @@
 from discord.ext import commands
 import discord
+from asyncio import sleep
+
+CHP_HELP = {
+    "ja": ("メッセージ自動公開機能。",
+"""# メッセージ自動公開プラグイン - autopublic
+これは`rt>autopublic <optionでcheck>`をニュースチャンネルのトピックに入れることで自動的にメッセージを公開してくれる機能です。  
+例：`rt>autopublic` (これをトピックに入れたチャンネルに送信したメッセージは全てメッセージを公開してくれます)
+例: `rt>autopublic ckeck` (これをトピックに入れたチャンネルに送信したメッセージは全てメッセージを公開してくれますが、公開するとメッセージにチェックが入ります)
+""")
 
 class Autopublic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        bot.loop.create_task(self.on_command_added())
+        
+    async def on_command_added(self):
+        await sleep(1.5)
+        for lang in CHP_HELP:
+            self.bot.cogs["DocHelp"].add_help(
+                "ChannelPlugin", "AutoPublic",
+                lang, *CHP_HELP[lang]
+            )
         
     @commands.Cog.listener()
     async def on_message(self, message):
