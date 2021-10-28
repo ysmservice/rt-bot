9db9b4e4a12260513a14d451e7a28c00893be478
tasuren <tasuren_0618@yahoo.co.jp>
2021-10-28 22:19:13 +0900

    update[coding]: 少し作った。

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -6,9 +6,11 @@ from discord.ext import commands
 import discord
 
 from tweepy.asynchronous import AsyncStream
+from aiohttp import client_exceptions
+from asyncio import sleep, Event
 
 if TYPE_CHECKING:
-    from asyncio import AbstractEventLoop, Task
+    from asyncio import AbstractEventLoop
     from tweepy.models import Status
     from aiomysql import Pool
     from rtlib import Backend
@@ -33,7 +35,7 @@ class DataManager:
                     );"""
                 )
                 await self._update_users(cursor)
-        self.start_stream()
+        self.ready.set()
 
     async def _read(self, cursor, channel, username):
         await cursor.execute(
@@ -83,14 +85,35 @@ class DataManager:
                 await self._update_users(cursor)
 
 
-class NewAsyncStream(AsyncStream):
+class TwitterNotification(commands.Cog, DataManager, AsyncStream):
 
+    TWITTERID_HEADERS = {
+        "authority": "tweeterid.com",
+        "sec-ch-ua": "^\\^Microsoft",
+        "accept": "*/*",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "x-requested-with": "XMLHttpRequest",
+        "sec-ch-ua-mobile": "?0",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.38",
+        "sec-ch-ua-platform": "^\\^Windows^\\^",
+        "origin": "https://tweeterid.com",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "referer": "https://tweeterid.com/",
+        "accept-language": "ja,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    }
     BASE_URL = "https://twitter.com/{}/status/{}"
 
-    def __init__(self, cog: "TwitterNotification", *args, **kwargs):
-        self.cog = cog
+    def __init__(self, bot: "Backend"):
+        self.bot = bot
+        self.users: Dict[str, int] = {}
+        self.ready: Event = Event()
+        super(commands.Cog, self).__init__(self.bot.loop, self.bot.mysql.pool)
+        super(DataManager, self).__init__(**self.bot.secret["twitter"])
         self.connected = False
-        super().__init__(*args, **kwargs)
+        self.cache: Dict[str, str] = {}
+        self.bot.loop.create_task(self.start_stream())
 
     def filter(self, *args, **kwargs):
         self.connected = True
@@ -100,9 +123,36 @@ class NewAsyncStream(AsyncStream):
         self.connected = False
         super().disconnect(*args, **kwargs)
 
+    async def get_user_id(self, username: str, retry: bool = False) -> str:
+        "指定されたユーザーのIDを取得する。"
+        print(1, username)
+        if username in self.cache:
+            return self.cache[username]
+        else:
+            try:
+                async with self.bot.session.post(
+                    "https://tweeterid.com/ajax.php",
+                    headers=self.TWITTERID_HEADERS, data={"input": username}
+                ) as r:
+                    if (user_id := await r.text()) == "error":
+                        return ""
+                    else:
+                        self.cache[username] = user_id
+                        print(2, user_id)
+                        return user_id
+            except client_exceptions.ClientOSError as e:
+                if retry:
+                    raise e
+                else:
+                    await sleep(1)
+                    return await self.get_user_id(username, True)
+
     async def on_status(self, status: "Status"):
+        # ツイートを取得した際に呼ばれる関数です。
+        print(status)
         if status.user.screen_name in self.users:
-            channel = self.cog.bot.get_channel(self.users[status.user.screen_name])
+            print(self.users[status.user.screen_name])
+            channel = self.bot.get_channel(self.users[status.user.screen_name])
             if channel:
                 try:
                     await channel.send(
@@ -113,38 +163,34 @@ class NewAsyncStream(AsyncStream):
                     print("Error on TwitterAsyncStream:", e)
             else:
                 # もし通知するチャンネルが見当たらない場合はその設定を削除する。
-                await self.cog.delete(
+                await self.delete(
                     self.users[status.user.screen_name], status.user.screen_name
                 )
 
-
-class TwitterNotification(commands.Cog, DataManager, NewAsyncStream):
-
-    task: "Task"
-
-    def __init__(self, bot: "Backend"):
-        self.bot = bot
-        self.users: Dict[str, int] = {}
-        super(commands.Cog, self).__init__(self.bot.loop, self.bot.mysql.pool)
-        super(DataManager, self).__init__(self, **self.bot.secret["twitter"])
-
-    def start_stream(self, disconnect: bool = False) -> None:
+    async def start_stream(self, disconnect: bool = False) -> None:
         "Twitterのストリームを開始します。"
         if disconnect and self.connected:
             self.disconnect()
+        if hasattr(self, "ready"):
+            await self.ready.wait()
+            del self.ready
         if self.users:
-            self.task = self.filter(follow=list(self.users.values()))
+            self.filter(
+                follow=[
+                    await self.get_user_id(username) for username in self.users
+                ]
+            )
 
     def cog_unload(self):
         if self.connected:
             self.disconnect()
 
     @commands.group(aliases=["ツイッター", "tw"])
-    async def twitter_(self, ctx):
+    async def twitter(self, ctx):
         if not ctx.invoked_subcommand:
             await ctx.reply("使用方法が違います。 / It is used in different ways.")
 
-    @twitter_.command("set", aliases=["s", "設定"])
+    @twitter.command("set", aliases=["s", "設定"])
     @commands.has_permissions(manage_channels=True, manage_webhooks=True)
     @commands.cooldown(1, 60, commands.BucketType.channel)
     async def set_(self, ctx, onoff: bool, *, username):
@@ -168,7 +214,7 @@ class TwitterNotification(commands.Cog, DataManager, NewAsyncStream):
             self.start_stream(True)
             await ctx.reply("Ok")
 
-    @twitter_.command("list", aliases=["l", "一覧"])
+    @twitter.command("list", aliases=["l", "一覧"])
     async def list_(self, ctx):
         await ctx.reply(
             embed=discord.Embed(
