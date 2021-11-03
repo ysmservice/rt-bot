1b0ef083c270f3e125167fc52f77705849972f7c
tasuren <tasuren_0618@yahoo.co.jp>
2021-11-03 14:30:59 +0900

    rm: rewrite

--- cogs/servers/constants.py
+++
@@ -1,8 +0,0 @@
-# RT.servers - Constants
-
-DB: str = "Servers"     # データベースのテーブル名です。
-INTERVAL: int = 10800   # Raise(表示順位上げ)ができる頻度(秒)です。
-MAX_TAGS: int = 10      # サーバーにつけれるタグの数です。
-MAX_TAG: int = 15       # タグにつけれる名前の最大の長さです。
-MAX_DETAIL: int = 2000  # サーバーの説明の最大文字数です。
-DOMAIN: str = "servers" # サブドメインの名前です。
