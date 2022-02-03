e23338e8b65fbc9e121503c83b1a7bc93db4443b
tasuren <45121209+tasuren@users.noreply.github.com>
2022-02-04 07:30:41 +0900

    fix: timemachineでエラーを返信するようにした。

--- cogs/server_tool.py
+++ cogs/server_tool.py
@@ -256,8 +256,8 @@ class ServerTool(commands.Cog):
                     await ctx.reply(embed=e)
                     break
                 else:
-                    raise discord.HTTPException("さかのぼりすぎた。")
-            except discord.HTTPException:
+                    raise OverflowError("さかのぼりすぎた。")
+            except OverflowError:
                 await ctx.reply(
                     {"ja": "過去にさかのぼりすぎました。",
                     "en": "I was transported back in time to another dimension."}
