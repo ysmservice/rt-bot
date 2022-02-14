c87b0d52c2effc93b87dbae9004c1890fb47be9f
tasuren <tasuren_0618@yahoo.co.jp>
2022-02-14 20:07:45 +0900

    coding: rocationsの作りかけ。

--- cogs/rocations.py
+++ cogs/rocations.py
@@ -10,7 +10,8 @@ from time import time
 
 from discord.ext import commands, tasks
 
-from rtlib import RT, Table, Context
+from rtlib.slash import UnionContext
+from rtlib import RT, Table
 
 
 # データ型等
@@ -30,7 +31,11 @@ class Server(TypedDict):
 
 class RocationsData(Table):
     __allocation__ = "Guild"
-RocationsData.__annotations__ = Server.__annotations__
+    description: str
+    tags: list[str]
+    invite: str
+    nices: list[Nice]
+    raised: float
 RAISE_TIME = 14106 # 3時間55分06秒
 Servers = OrderedDict[int, Server]
 SIZE_PER_INDEX = 10
@@ -41,7 +46,7 @@ CheckFT = TypeVar("CheckFT")
 def check(function: CheckFT) -> CheckFT:
     "宣伝が有効になっているか"
     @wraps(function)
-    async def new(self: Rocations, ctx: Context, *args, **kwargs):
+    async def new(self: Rocations, ctx: UnionContext, *args, **kwargs):
         if ctx.guild.id in self.data:
             return await function(self, ctx, *args, **kwargs)
         else:
@@ -56,8 +61,8 @@ def check(function: CheckFT) -> CheckFT:
 class Rocations(commands.Cog):
     def __init__(self, bot: RT):
         self.bot = bot
-        self.data = Server(bot)
-        self._sorted_cache: list[Servers] = {}
+        self.data = RocationsData(bot)
+        self._sorted_cache: list[Servers] = []
 
         self.bot.rtc.set_event(self.get_rocations)
         self._process_cache.start()
@@ -65,10 +70,10 @@ class Rocations(commands.Cog):
     @tasks.loop(seconds=10)
     async def _process_cache(self):
         # キャッシュを処理します。
-            # 十個ずつRaisedされた順でサーバー一覧のキャッシュを作っていく。
+        # 十個ずつRaisedされた順でサーバー一覧のキャッシュを作っていく。
         i, now = 0, OrderedDict()
         for guild_id, data in sorted(
-            list(self.data.to_dict().items()), key=lambda x: x[0].get("raised", 0.0)
+            list(self.data.to_dict().items()), key=lambda x: x[1].get("raised", 0.0)
         ):
             i += 1
             now[guild_id] = data
@@ -89,51 +94,52 @@ class Rocations(commands.Cog):
 
     def get_rocations(self, page: int) -> Optional[Servers]:
         "指定されたページのRocationsを取得します。"
-        try:
-            return self._sorted_cache[page]
-        except KeyError:
-            ...
+        try: return self._sorted_cache[page]
+        except KeyError: ...
 
     @commands.group()
     @commands.cooldown(1, 5, commands.BucketType.guild)
-    async def rocations(self, ctx: Context):
-        if ctx.invoked_subcommand:
-            await ctx.reply("Ok")
-        else:
+    async def rocations(self, ctx: UnionContext):
+        if not ctx.invoked_subcommand:
             await ctx.reply("使用方法が違います。")
 
     @rocations.command()
     @commands.cooldown(1, 15, commands.BucketType.guild)
-    async def toggle(self, ctx: Context):
+    async def toggle(self, ctx: UnionContext):
         if ctx.guild.id in self.data:
             del self.data[ctx.guild.id]
+            await ctx.reply("Offにしました。")
         else:
-            self.data[ctx.guild.id]
+            self.data[ctx.guild.id].description = ""
+            await ctx.reply("Onにしました。")
 
     @rocations.command()
     @check
-    async def tags(self, ctx: Context, *, tags: str):
+    async def tags(self, ctx: UnionContext, *, tags: str):
         self.data[ctx.guild.id].tags = tags.split("/")
+        await ctx.reply("Ok")
 
     @rocations.command()
     @check
-    async def description(self, ctx: Context, *, description: str):
+    async def description(self, ctx: UnionContext, *, description: str):
         self.data[ctx.guild.id].description = description
+        await ctx.reply("Ok")
 
     @rocations.command()
     @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def invite(self, ctx: Context):
+    async def invite(self, ctx: UnionContext):
         self.data[ctx.guild.id].invite = (
             await ctx.guild.vanity_invite()
             or await ctx.channel.create_invite(
                 reason="Rocationに登録する招待リンクの作成のため。"
             )
         ).url
+        await ctx.reply("Ok")
 
     @rocations.command("raise")
     @commands.cooldown(1, 10, commands.BucketType.user)
     @check
-    async def raise_(self, ctx: Context):
+    async def raise_(self, ctx: UnionContext):
         if (elapsed := (now := time()) - self.data[ctx.guild.id].get("raised", 0.0)) > RAISE_TIME:
             self.data[ctx.guild.id].raised = now
             await ctx.reply("Raised! 表示順位をあげました。\n※数秒後反映されます。")
@@ -143,7 +149,7 @@ class Rocations(commands.Cog):
     @commands.command("raise")
     @commands.cooldown(1, 10, commands.BucketType.user)
     @check
-    async def raise_alias(self, ctx: Context):
+    async def raise_alias(self, ctx: UnionContext):
         await self.raise_(ctx)
 
 
