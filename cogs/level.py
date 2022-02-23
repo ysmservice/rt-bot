09e53fe548f4664644045acb50c6f1b2b5bbc79f
音夢?! <82650472+Nemu627@users.noreply.github.com>
2022-02-23 17:16:55 +0900

    add : ヘルプにパラメーターの説明追加
    sinfoコマンドのヘルプの要素パクった

--- cogs/level.py
+++ cogs/level.py
@@ -65,13 +65,20 @@ class Level(commands.Cog):
             }
         }
     )
-    async def level(self, ctx: commands.Context, user_name_id = None):
+    async def level(self, ctx: commands.Context, user_id = None):
         """!lang ja
         --------
         レベル機能です。RTのいるサーバーで一定数しゃべるとレベルが上がります。
         グローバルレベルとローカルレベルという2種類のレベリング方式があります。
         グローバルレベルは共通で、全世界での自分の位置を知ることができます。
         ローカルレベルはサーバーごとで管理(一定レベルに達したら役職をあげるなど)できます。
+        
+        Parameters
+        ----------
+        user_id : int, optional
+            対象のユーザーのIDです。  
+            指定しなかった場合はコマンドを実行したユーザーとなります。  
+            RTのいるユーザーしかユーザー指定はできません。
 
         Aliases
         -------
@@ -87,8 +94,8 @@ class Level(commands.Cog):
         Aliases
         -------
         lv"""
-        if user_name_id is None:
-            user_name_id = ctx.author.id
+        if user_id is None:
+            user_id = ctx.author.id
         if not ctx.invoked_subcommand:
             await ctx.reply(
                 embed=discord.Embed(
