5877268e101858fb9df620db9024ebee5687cbff
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-11 07:49:21 +0900

    Update log.py

--- cogs/log.py
+++ cogs/log.py
@@ -118,7 +118,7 @@ class Log(commands.Cog):
     @log()
     async def on_message_edit(self, before, after):
         embed = discord.Embed(title="メッセージ編集", color=self.c)
-        embed.set_author(icon_url=getattr(after.author.icon, "url"))
+        embed.set_author(icon_url=getattr(after.author.icon, "url", ""))
         embed.add_field(name="Before", value=before.content)
         embed.add_field(name="After", value=after.content)
         return embed
