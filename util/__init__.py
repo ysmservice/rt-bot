6dfe871d59217f8119f32adf7bab1277e94a4053
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-04-12 16:31:55 +0900

    update: converter作成に合わせ更新

--- util/__init__.py
+++ util/__init__.py
@@ -1,9 +1,24 @@
 # RT Utilities
 
 from .checks import isintable, has_any_roles, has_all_roles
+from .converters import (
+    MembersConverter,
+    UsersConverter,
+    TextChannelsConverter,
+    VoiceChannelsConverter,
+    RolesConverter,
+    ObjectsConverter
+)
+
 
 __all__ = [
     "isintable",
     "has_any_roles",
-    "has_all_roles"
+    "has_all_roles",
+    "MembersConverter",
+    "UsersConverter",
+    "TextChannelsConverter",
+    "VoiceChannelsConverter",
+    "RolesConverter",
+    "ObjectsConverter"
 ]
