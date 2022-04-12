879ad763fe2ea57c95d993a75a2be48911c952a5
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-04-12 16:29:57 +0900

    Create converters.py

---
+++ util/converters.py
@@ -0,0 +1,36 @@
+# rtutil - Converters
+
+from discord.ext import commands
+
+
+class Multiple(commands.Converter):
+    "`, `で区切って_originalでconvertしたlistを返すConverterの抽象クラスです。"
+
+    _original: commands.Converter
+
+    async def convert(self, ctx: commands.Context, arg: str):
+        return [await self._original().convert(ctx, word) for word in arg.split(", ")]
+
+
+class MembersConverter(Multiple):
+    _original = commands.MemberConverter
+
+
+class UsersConverter(Multiple):
+    _original = commands.UserConverter
+
+
+class TextChannelsConverter(Multiple):
+    _original = commands.TextChannelConverter
+
+
+class VoiceChannelsConverter(Multiple):
+    _original = commands.VoiceChannelConverter
+
+
+class RolesConverter(Multiple):
+    _original = commands.RoleConverter
+
+
+class ObjectsConverter(Multiple):
+    _original = commands.ObjectConverter
