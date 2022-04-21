862b09dac1091d16d2c4259b8599f9491adc6432
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-04-21 17:02:31 +0900

    self.bot.sessionに変更

--- cogs/gamesearch.py
+++ cogs/gamesearch.py
@@ -12,24 +12,23 @@ class gamesearch(commands.Cog):
 
     @commands.command(name="gamesearch")
     async def transit(self, ctx, *, name):
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://ysmsrv.wjg.jp/disbot/gamesearch.php?q="+urllib.parse.quote_plus(name, encoding='utf-8')) as resp:
-                sid = await resp.text()
-                gj = json.loads(sid)
-                hdw = ""
-                try:
-                   game = gj["Items"][0]
-                   gametitle = game["Item"]["titleKana"]
-                   for item in gj["Items"]:
-                      if gametitle in item["Item"]["titleKana"]:
-                         hdw = hdw + " " + item["Item"]["hardware"]
-                   embed = discord.Embed(title=gametitle + "の詳細", description=game["Item"]["itemCaption"].replace('\\n','\n'), color=0x0066ff)
-                   embed.add_field(name="機種", value=hdw)
-                   embed.set_image(url=game["Item"]["largeImageUrl"])
-                   embed.set_footer(text="ゲーム情報検索")
-                   await ctx.send(embed=embed)
-                except IndexError:
-                   await ctx.send("すみません。見つかりませんでした。別の単語をお試しください")
+        async with self.bot.session.get("https://ysmsrv.wjg.jp/disbot/gamesearch.php?q="+urllib.parse.quote_plus(name, encoding='utf-8')) as resp:
+            sid = await resp.text()
+            gj = json.loads(sid)
+            hdw = ""
+            try:
+               game = gj["Items"][0]
+               gametitle = game["Item"]["titleKana"]
+               for item in gj["Items"]:
+                  if gametitle in item["Item"]["titleKana"]:
+                     hdw = hdw + " " + item["Item"]["hardware"]
+               embed = discord.Embed(title=gametitle + "の詳細", description=game["Item"]["itemCaption"].replace('\\n','\n'), color=0x0066ff)
+               embed.add_field(name="機種", value=hdw)
+               embed.set_image(url=game["Item"]["largeImageUrl"])
+               embed.set_footer(text="ゲーム情報検索")
+               await ctx.send(embed=embed)
+            except IndexError:
+               await ctx.send("すみません。見つかりませんでした。別の単語をお試しください")
 
 
 
