f298e3faaeb5b8c0d21a1014642529d97a74caff
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-04-23 00:12:47 +0900

    command_logsをdevelopのサブコマンドとして作成した。

--- cogs/develop.py
+++ cogs/develop.py
@@ -10,7 +10,12 @@ class Develop(commands.Cog):
     def __init__(self, bot: RT):
         self.bot = bot
     
-    @commands.group()
+    @commands.group(
+        extras={
+            "headding":{"ja":"管理者用のコマンドです。", "en":"Only for developers command."},
+            "parent":"Admin"
+        }
+    )
     @commands.is_owner()
     async def develop(self, ctx):
         if not ctx.invoked_subcommand is None:
@@ -26,6 +31,47 @@ class Develop(commands.Cog):
             for c in [self.bot.get_command(co) for co in command_name.split()]:
                 await self.bot.cogs["DocHelp"].on_command_add(c)
             await ctx.send(f"{', '.join(command_name.split())}のhelp読み込み完了")
+
+    @develop.command(
+        extras={
+            "headding":{"ja":"直近1分間のコマンド実行ログを見ます。", "en":"View commands logs."}
+        }
+    )
+    @commands.is_owner()
+    async def command_logs(self, ctx, mode=None):
+        """!lang ja
+        --------
+        直近1分間のコマンド実行ログを見ることができます。また、実行ログのループ操作もできます。
+        
+        Parameters
+        ----------
+        mode: startやstop、restartなど
+            logging_loop.○○の○○に入れられる文字列を入れて下さい。
+        
+        Warnings
+        --------
+        もちろん実行は管理者専用です。
+        
+        !lang en
+        --------
+        View command logs. Also it can control loop of logs.
+        
+        Parameters
+        ----------
+        mode: start/stop, or restart
+            Put the string which can be put in logging_loop.●●.
+        
+        Warnings
+        --------
+        Of cource it can only be used by admin.
+        """
+        if mode:
+            getattr(self.bot.cogs["SystemLog"].logging_loop, mode)()
+            await ctx.message.add_reaction("✅")
+        elif len(self.bot.cogs["SystemLog"].names) != 0:
+            await ctx.reply(embed=self.bot.cogs["SystemLog"]._make_embed())
+        else:
+            await ctx.reply("ログ無し。")
 
 
 def setup(bot):
