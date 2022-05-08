53ed7fc591f599446f22beef6ba4600ffa936611
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-05-08 15:30:44 +0900

    fix: sつけ忘れた(

--- cogs/language.py
+++ cogs/language.py
@@ -93,7 +93,7 @@ class Language(commands.Cog):
             args = (self.get_text(args[0], lang),)
         if kwargs.get("embed", False):
             kwargs["embed"] = self.get_text(kwargs["embed"], lang)
-        if kwargs.get("embed", False):
+        if kwargs.get("embeds", False):
             kwargs["embeds"] = [self.get_text(embed, lang)
                                 for embed in kwargs.get("embeds", [])]
 
