ba9339fa9a9dd525ca2a281a0808aeec3a488e08
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-05-10 13:39:59 +0900

    fix: discordをimportするようにした。

--- util/dpy_monkey.py
+++ util/dpy_monkey.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from discord.ext import commands
+import discord
 
 from .webhooks import webhook_send
 from .cacher import CacherPool
