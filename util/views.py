1add9d6845373f28b9d8acd44e1e0ae22db21d35
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-05-11 17:21:41 +0900

    fix: バグ修正

--- util/views.py
+++ util/views.py
@@ -1,12 +1,15 @@
 # Free RT Util - Views
 
 import discord
-
+import platform
 
 class TimeoutView(discord.ui.View):
     "タイムアウト時にコンポーネントを使用不可に編集するようにするViewです。"
 
-    message: discord.Message | None = None
+    if platform.python_version_tuple()[0]=='3' and int(platform.python_version_tuple()[1])>9:
+        message: discord.Message | None = None
+    else:
+        message: discord.Message
 
     async def on_timeout(self):
         for child in self.children:
