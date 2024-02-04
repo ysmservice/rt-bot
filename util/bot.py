439f210a65c902b3d54921921fd8cfd31dc1d5e8
ysmreg <33682244+ysmreg@users.noreply.github.com>
2024-02-04 21:16:04 +0900

    update: bot

--- util/bot.py
+++ util/bot.py
@@ -59,8 +59,8 @@ class RT(commands.AutoShardedBot):
         temp = [*args]
         if len(args) >= 1 and args[0].startswith("[") and args[0].endswith("]"):
             temp[0] = f"\033[93m{args[0]}\033[0m"
-        if len(args) >= 2 and args[1].startswith("[") and args[1].endswith("]"):
-            temp[1] = f"\033[95m{args[1]}\033[0m"
+        if len(args) >= 2 and str(args[1]).startswith("[") and str(args[1]).endswith("]"):
+            temp[1] = f"\033[95m{str(args[1])}\033[0m"
         return print("\033[32m[RT log]\033[0m", *temp, **kwargs)
 
     def get_ip(self) -> str:
